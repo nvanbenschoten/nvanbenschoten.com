6d0c9ed878030850741456e91d4fb0e76c620bdc
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2014-07-22 22:29:27 -0400

    Begin setting up website with working index.html and main controller

--- config/express.js
+++ config/express.js
@@ -3,11 +3,17 @@
 /**
  * Module dependencies
  */
-var express = require('express');
+var express = require('express'),
+       path = path('path');
 
 module.exports = function() {
     // Initialize express app
     var app = express();
+
+    // Globbing model files
+    config.getGlobbedFiles('./app/models/**/*.js').forEach(function(modelPath) {
+        require(path.resolve(modelPath));
+    });
 
     return app;
 };
